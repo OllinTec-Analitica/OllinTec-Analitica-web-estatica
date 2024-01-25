fd22c22e1a1c5a1fc61d6623bdf6bc04ab84eff9
luislmv <muniz.valledor.luis@ciencias.unam.mx>
2024-01-25 02:14:49 -0600

    Corregir commit previo
    Se ajustó mejor un parámetro y se agregaron los cambios en archivos que
    no fueron incluidos en el anterior commit por error.

--- js/menu.js
+++ js/menu.js
@@ -35,3 +35,47 @@ window.addEventListener("scroll", function() {
         lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
     }
 }, false);
+
+function ajustarIndicadores() {
+    const indicadorArriba = document.getElementById('scroll-up-indicator');
+    const indicadorAbajo = document.getElementById('scroll-down-indicator');
+    const posicionInicial = navbar.offsetTop;
+    const navList = document.getElementById('nav-list');
+
+    if (window.innerWidth > 900 || window.innerHeight > 370) {
+        // Ocultar ambos indicadores si la lista no es desplazable
+        indicadorArriba.style.display = 'none';
+        indicadorAbajo.style.display = 'none';
+    } else {
+        navList.scrollTo({
+            top: posicionInicial
+        });
+        indicadorAbajo.style.display = 'block';
+    }
+}
+
+// Evento de redimensionamiento de la ventana
+window.addEventListener('resize', ajustarIndicadores);/**/
+
+
+document.getElementById('nav-list').addEventListener('scroll', function() {
+    var navList = this;
+    var scrollUpIndicator = document.getElementById('scroll-up-indicator');
+    var scrollDownIndicator = document.getElementById('scroll-down-indicator');
+
+    const tolerancia = 60; // Un pequeño margen de tolerancia en píxeles
+
+    // Mostrar/Ocultar el indicador superior
+    if (Math.round(navList.scrollTop) > tolerancia) {
+        scrollUpIndicator.style.display = 'block';
+    } else {
+        scrollUpIndicator.style.display = 'none';
+    }
+
+    // Mostrar/Ocultar el indicador inferior
+    if (Math.round(navList.scrollHeight - navList.clientHeight - navList.scrollTop) > tolerancia) {
+        scrollDownIndicator.style.display = 'block';
+    } else {
+        scrollDownIndicator.style.display = 'none';
+    }
+});
